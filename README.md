d51a643275cba1a07d3b6f24a78b6f73e4cfd409
mtatiana115 <mtatiana115@gmail.com>
2024-04-24 08:24:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# _05_eventLog-sBoot
---
+++ src/test/java/com/riwi/eventLog/EventLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.eventLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
