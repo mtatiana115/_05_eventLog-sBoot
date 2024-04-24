9d15581d220cc179a0ed9e676a94b0521387704e
mtatiana115 <mtatiana115@gmail.com>
2024-04-24 09:19:36 -0500

    feature/repository with find by name added

---
+++ src/main/java/com/riwi/eventLog/repositories/EventRepository.java
@@ -0,0 +1,13 @@
+package com.riwi.eventLog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.riwi.eventLog.entities.Event;
+
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+  //creo el método
+  public Event findByName(String name);
+}
